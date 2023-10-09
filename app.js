1e3aecf28ca7fcaa4b20ed6062620b6ad622dab2
leafmllr <lea.feline@web.de>
2023-10-09 18:05:59 +0200

    revised database, added skills and users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const session = require ('express-session'); // middleware for handling session data
 const connectSqlite3 = require ('connect-sqlite3');
 const cookieParser = require ('cookie-parser'); // middleware to parse cookies & making them accessible
+const bcrypt = require('bcrypt'); // middleware for hashing passwords
 
 
 const port = 8080 // defines the port
@@ -97,7 +98,7 @@ app.use(session({
 const db = new sqlite3.Database('portfolio-lfm.db')
 
 // creates table projects at startup
-db.run("CREATE TABLE projects (projectid INTEGER PRIMARY KEY AUTOINCREMENT, projectTitle TEXT NOT NULL, projectDesc TEXT NOT NULL, projectDate INTEGER NOT NULL, projectImgURL TEXT NOT NULL)", (error) => {
+db.run("CREATE TABLE projects (projectid INTEGER PRIMARY KEY AUTOINCREMENT, projectTitle TEXT NOT NULL, projectDesc TEXT NOT NULL, projectDate INTEGER NOT NULL, projectImgURL TEXT NOT NULL, categoryfk INTEGER NOT NULL)", (error) => {
   if (error) {
     console.log("CREATING ERROR: ", error)
   } else {
@@ -106,47 +107,55 @@ db.run("CREATE TABLE projects (projectid INTEGER PRIMARY KEY AUTOINCREMENT, proj
         "title":"'Der Hahn ist tot' | 'The Rooster is Dead'",
         "desc":"Development and production of a short film. In a group of six people,  I conceptualised and realised the 5-minute comedy 'The Rooster is Dead' from the script to the final cut, focusing on camera and lighting,  production design, colour grading and title design in post-production.",
         "date":"2023-03",
-        "url":"public/img/theRoosterisDead.jpg"},
+        "url":"theRoosterisDead.jpg",
+        "categoryfk":"6"},
       {"id":"2",
         "title":"Room of Inspiration",
-        "desc":"",
+        "desc":"My vision for a retreat in ten years seamlessly combines the charm of an old building with the contemporary flair of a loft. Created using Blender for 3D modelling and Adobe Substance Painter for textures, this self-designed space features vintage furniture, open windows, hanging lamps, a spiral staircase and warm evening tones. It's a place to recharge and create, and a glimpse of a future retreat where craft space and design merge for inspiration.",
         "date":"2023-06",
-        "url":"public/img/RoomOfInspiration.jpg"},
+        "url":"RoomOfInspiration.jpg",
+        "categoryfk":"1"},
       {"id":"3",
         "title":"Fischbude",
-        "desc":"",
+        "desc":"Designing the 'Fischbude' logo was a journey into seaside simplicity. Using classic blue and white tones, we captured the essence of fresh seafood. The playful fish icon at the heart of the design embodies the welcoming coastal charm of this eatery, promising a delicious seafood experience.",
         "date":"2023-06",
-        "url":"public/img/Fischbude.jpg"},
+        "url":"Fischbude.jpg",
+        "categoryfk":"4"},
       {"id":"4",
         "title":"FancyThat!",
-        "desc":"",
+        "desc":"Enter the world of 'Fancy That!' and discover a brand dedicated to making life easier and better. The project showcased the brand's eyewear collection while reinforcing the its distinctive identity. A modern UX/UI design for the frontend aims to create an inviting and fresh experience. In addition, a backend system with seamless database integration and the use of handlebars allows users to manage and update content independently.",
         "date":"2022-12",
-        "url":"public/img/FancyThat.jpg"},
+        "url":"FancyThat.jpg",
+        "categoryfk":"3"},
       {"id":"5",
         "title":"Love is a Journey",
         "desc":"Conception and realisation of a wedding magazine. Creative staging of wedding greetings with the look and feel of a 'travel journal' – the special feature here was the illustrative presentation of travel recommendations through graphics, fonts and images on almost 65 pages.",
         "date":"2021-07",
-        "url":"public/img/LoveIsAJourney.jpg"},
+        "url":"LoveIsAJourney.jpg",
+        "categoryfk":"5"},
       {"id":"6",
         "title":"Campusmagazine viel. | 26",
         "desc":"",
         "date":"2023-02",
-        "url":"public/img/Campusmagazine-viel_26.jpg"},
+        "url":"vorlage_projects.jpg",
+        "categoryfk":"5"},
       {"id":"7",
         "title":"Campusmagazine viel. | 27",
-        "desc":"",
+        "desc":"As part of a team of five, I was involved in the editorial design of the campus magazine of the Kiel University of Applied Sciences. Our work included the conception and design of columns, polls and photo series, all centred around the theme of 'colour'. The challenge was to seamlessly integrate colour as a core element in the design of each page, ensuring a harmonious and effective reading experience.",
         "date":"2023-08",
-        "url":"public/img/Campusmagazine-viel_27.jpg"},
+        "url":"Campusmagazine-viel_27.jpg",
+        "categoryfk":"5"},
       {"id":"8",
         "title":"Wedding invitation card",
-        "desc":"",
+        "desc":"This wedding invitation card beautifully combines the beauty of autumn with a touch of elegance, using the vibrant colours of the season. Nature-inspired accents, including leaf motifs, are thoughtfully combined with clean and simple typography. The result is a harmonious balance of simplicity and playfulness that sets the perfect tone for an autumnal wedding celebration.",
         "date":"2023-08",
-        "url":"public/img/WeddingCard_SaTo.jpg"},
+        "url":"WeddingCard_SaTo.jpg",
+        "categoryfk":"8"},
     ]
 
     // insert projects
     projects.forEach ( (oneProject) => {
-      db.run("INSERT INTO projects (projectid, projectTitle, projectDesc, projectDate, projectImgURL) VALUES (?, ?, ?, ?, ?)", [oneProject.id, oneProject.title, oneProject.desc, oneProject.date, oneProject.url], (error) => {
+      db.run("INSERT INTO projects (projectid, projectTitle, projectDesc, projectDate, projectImgURL, categoryfk) VALUES (?, ?, ?, ?, ?, ?)", [oneProject.id, oneProject.title, oneProject.desc, oneProject.date, oneProject.url, oneProject.categoryfk], (error) => {
         if (error) {
           console.log("INSERT ERROR: ", error)
         } else {
@@ -271,41 +280,28 @@ db.run ("CREATE TABLE categories (categoryid INTEGER PRIMARY KEY AUTOINCREMENT, 
   }
 })
 
-// creates table projectsCategories at startup
-db.run("CREATE TABLE projectsCategories (procatid INTEGER PRIMARY KEY AUTOINCREMENT, projectid INTEGER, categoryid INTEGER, FOREIGN KEY (projectid) REFERENCES projects (projectid), FOREIGN KEY (categoryid) REFERENCES categories (categoryid))", (error) => {
+// creates table users at startup
+db.run ("CREATE TABLE users (userID INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT NOT NULL, userPassword TEXT NOT NULL)", (error) => {
   if(error) {
     console.log("ERROR: ", error)
   } else {
-    const projectsCategories = [
-      {"id":"1", "projectid":"1", "categoryid": "6"},
-      {"id":"2", "projectid":"1", "categoryid": "8"},
-      {"id":"3", "projectid":"2", "categoryid": "1"},
-      {"id":"4", "projectid":"3", "categoryid": "8"},
-      {"id":"5", "projectid":"3", "categoryid": "2"},
-      {"id":"6", "projectid":"3", "categoryid": "4"},
-      {"id":"7", "projectid":"4", "categoryid": "3"},
-      {"id":"8", "projectid":"4", "categoryid": "4"},
-      {"id":"9", "projectid":"5", "categoryid": "5"},
-      {"id":"10", "projectid":"5", "categoryid": "8"},
-      {"id":"11", "projectid":"6", "categoryid": "5"},
-      {"id":"12", "projectid":"6", "categoryid": "8"},
-      {"id":"13", "projectid":"7", "categoryid": "5"},
-      {"id":"14", "projectid":"7", "categoryid": "8"},
-      {"id":"15", "projectid":"8", "categoryid": "8"},
+    const users = [
+      {"id":"1", "name":"leafmllr", "password":"5678"},
+      {"id":"2", "name":"christoffel", "password":"1234"},
     ]
 
-    // insert projectsCategories
-    projectsCategories.forEach( (oneProjectCategory) => {
-      db.run("INSERT INTO projectsCategories (procatid, projectid, categoryid) VALUES (?, ?, ?)", [oneProjectCategory.id, oneProjectCategory.projectid, oneProjectCategory.categoryid], (error) => {
+    // insert users
+    users.forEach( (oneUser) => {
+      db.run("INSERT INTO users (userID, userName, userPassword) VALUES (?, ?, ?)", [oneUser.id, oneUser.name, oneUser.password], (error) => {
         if(error) {
           console.log("ERROR: ", error)
         } else {
-          console.log("Line added into the projectsCategories table!")
+          console.log("Line added into the users table!")
         }
       })
     })
 
-    console.log("Table projectsCategories created successfully!")
+    console.log("Table users created successfully!")
   }
 })
 
@@ -325,7 +321,7 @@ db.run("CREATE TABLE projectsCategories (procatid INTEGER PRIMARY KEY AUTOINCREM
 
 // defines route "/"
 app.get('/', (request, response) => {
-  db.all("SELECT * FROM projects", function (error, theProjects) {
+  db.all("SELECT * FROM projects JOIN categories ON projects.categoryfk = categories.categoryid", function (error, theProjects) {
     if(error) {
       const model = {
         dbError: true,
@@ -380,6 +376,7 @@ app.get('/contact', (request, response) => {
 
 
 
+
 // defines route "/login"
 app.get('/login', (request, response) => {
   const model = {
@@ -396,21 +393,23 @@ app.post('/login', (request, response) => {
   const un = request.body.un
   const pw = request.body.pw
 
-  if(un=="leafmllr" && pw=="5678") {
-    console.log("Welcome Lea!")
-
-    request.session.isAdmin = true;
-    request.session.isLoggedIn = true;
-    request.session.name = "Lea";
-    response.redirect('/');
-  } else {
-    console.log("Nice try!")
-
-    request.session.isAdmin = false;
-    request.session.isLoggedIn = false;
-    request.session.name = "";
-    response.redirect('/login');
-  }
+  db.all("SELECT * FROM users WHERE userName=? AND userPassword=?", [un], [pw], function(error, User) {
+    if (error) {
+      console.log("Nice try!")
+  
+      request.session.isAdmin = false;
+      request.session.isLoggedIn = false;
+      request.session.name = "";
+      response.redirect('/login');
+    } else {
+      console.log("Welcome!")
+  
+      request.session.isAdmin = true;
+      request.session.isLoggedIn = true;
+      request.session.name = un;
+      response.redirect('/');
+    }
+  })
 });
 
 // defines route "/logout"
@@ -427,13 +426,14 @@ app.get('/logout', (request, response) => {
 
 
 
+
 // defines route of a single project
 app.get('/:id', (request, response) => {
 
   // get the id on the dynamic route
   const id = request.params.id
 
-  db.all("SELECT * FROM projects WHERE projectid=?", [id], function(error, selectedProject) {
+  db.all("SELECT * FROM projects JOIN categories ON projects.categoryfk = categories.categoryid WHERE projectid=? ", [id], function(error, selectedProject) {
 
     if(error) {
       const model = {
@@ -477,8 +477,9 @@ app.get('/:id/delete', (request, response) => {
           isAdmin: request.session.isAdmin,
           isLoggedIn: request.session.isLoggedIn,
           name: request.session.name,
+          projectDeletion: true,
         }
-        response.render('portfolio.handlebars', model)
+        response.redirect('/adminfeedback', model)
       } else {
         const model = {
           dbError: false,
@@ -486,6 +487,7 @@ app.get('/:id/delete', (request, response) => {
           isAdmin: request.session.isAdmin,
           isLoggedIn: request.session.isLoggedIn,
           name: request.session.name,
+          projectDeletion: false,
         }
         response.render('portfolio.handlebars', model)
       } 
@@ -582,6 +584,19 @@ app.post('/:id/update', (request, response) => {
     response.redirect ('/login')
   }
 })
+
+// defines route "/adminfeedback"
+app.get('/adminfeedback', (request, response) => {
+  const model = {
+    isAdmin: request.session.isAdmin,
+    isLoggedIn: request.session.isLoggedIn,
+    name: request.session.name,
+    projectDeletion: request.session.projectDeletion,
+  }
+
+  // rendering the view
+  response.render('adminfeedback.handlebars', model);
+});
 
 
 
